3b39f54eb80d1cc3390a3c6ad5c8e0ce21d40f5e
Avi <avishkarkale29@gmail.com>
2026-04-11 22:38:55 +0530

    Implement Index Buffers to draw a rectangle

--- src/main.cpp
+++ src/main.cpp
@@ -65,9 +65,15 @@ struct Vertex{
 };
 
 const std::vector<Vertex> vertices ={
-    {{0.0f, -0.5f},{1.0f,1.0f, 1.0f}},//top corner
-    {{0.5f,0.5f},{0.0f,1.0f,0.0f}},//bottom right
-    {{-0.5f,0.5f},{0.0f,0.0f,1.0f}}//bottom left
+    {{-0.5f,-0.5f},{1.0f,0.0f, 0.0f}},//top left
+    {{0.5f,-0.5f}, {0.0f,1.0f,0.0f}},//top right
+    {{0.5f,0.5f},  {0.0f,0.0f,1.0f}},//bottom right
+    {{-0.5f,0.5f}, {1.0f,1.0f,1.0f}} // bottom left
+};
+
+const std::vector<uint16_t> indices = {
+    0,1,2, 
+    2,3,0
 };
 class HelloTriangleApplication {
     public:
@@ -118,6 +124,9 @@ class HelloTriangleApplication {
       VkFence inFlightFence;
       VkBuffer vertexBuffer;
       VkDeviceMemory vertexBufferMemory;
+
+      VkBuffer indexBuffer;
+      VkDeviceMemory indexBufferMemory;
 
      struct QueueFamilyIndices{
         std::optional<uint32_t> graphicsFamily;
@@ -191,6 +200,7 @@ class HelloTriangleApplication {
         createFramebuffers();
         createCommandPool();
         createVertexBuffer();
+        createIndexBuffer();
         createCommandBuffer();
         createSyncObjects();
 
@@ -851,7 +861,27 @@ copyBuffer(stagingBuffer , vertexBuffer , bufferSize);
 
 vkDestroyBuffer(device , stagingBuffer , nullptr);
 vkFreeMemory(device , stagingBufferMemory , nullptr);
-}    
+}
+
+void createIndexBuffer(){
+    VkDeviceSize bufferSize = sizeof(indices[0] )* indices.size();
+
+    VkBuffer stagingBuffer;
+    VkDeviceMemory stagingBufferMemory;
+    createBuffer(bufferSize , VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |VK_MEMORY_PROPERTY_HOST_COHERENT_BIT , stagingBuffer, stagingBufferMemory);
+
+    void* data;
+    vkMapMemory(device , stagingBufferMemory , 0 , bufferSize , 0 , &data);
+      memcpy(data , indices.data(), (size_t)bufferSize);
+    vkUnmapMemory(device , stagingBufferMemory);
+    
+    createBuffer(bufferSize , VK_BUFFER_USAGE_TRANSFER_DST_BIT |VK_BUFFER_USAGE_INDEX_BUFFER_BIT , VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT , indexBuffer, indexBufferMemory);
+
+    copyBuffer(stagingBuffer , indexBuffer , bufferSize);
+
+    vkDestroyBuffer(device , stagingBuffer , nullptr);
+    vkFreeMemory(device , stagingBufferMemory , nullptr);
+}
 
  void createCommandPool(){
     QueueFamilyIndices queueFamilyIndices = findQueueFamilies(physicalDevice);
@@ -913,6 +943,8 @@ vkFreeMemory(device , stagingBufferMemory , nullptr);
     VkBuffer vertexBuffers[] = {vertexBuffer};
     VkDeviceSize offsets[] = {0};
     vkCmdBindVertexBuffers(commandBuffer, 0 , 1 , vertexBuffers, offsets);
+    
+    vkCmdBindIndexBuffer(commandBuffer, indexBuffer , 0 , VK_INDEX_TYPE_UINT16);
     //Dynamic States
     VkViewport viewport{};
     viewport.x = 0.0f;
@@ -929,7 +961,7 @@ vkFreeMemory(device , stagingBufferMemory , nullptr);
     scissor.extent = swapChainExtent;
     vkCmdSetScissor(commandBuffer,0,1, &scissor);
 
-    vkCmdDraw(commandBuffer, 3,1,0,0);//Draw the triangle
+    vkCmdDrawIndexed(commandBuffer,static_cast<uint32_t>(indices.size()), 1,0,0,0);// drawing rectangle
 
     vkCmdEndRenderPass(commandBuffer);//finish render pass
 
@@ -1153,6 +1185,8 @@ void createInstance(){
             vkDestroyImageView(device, imageView, nullptr);
         }
         vkDestroyBuffer(device, vertexBuffer , nullptr);
+        vkDestroyBuffer(device, indexBuffer , nullptr);
+        vkFreeMemory(device , indexBufferMemory, nullptr);
         vkFreeMemory(device , vertexBufferMemory, nullptr);
         vkDestroyPipeline(device, graphicsPipeline,nullptr);
         vkDestroyPipelineLayout(device , pipelineLayout, nullptr);
