d93f0ba93e5eaa1bd86961fb731d011d5a8c65a0
Avi <avishkarkale29@gmail.com>
2026-04-25 00:22:57 +0530

    Implement create Image helper function

--- src/main.cpp
+++ src/main.cpp
@@ -915,6 +915,40 @@ uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties){
 
  }
 
+ void createImage(uint32_t width , uint32_t height , VkFormat format , VkImageTiling tiling , VkImageUsageFlags usage , VkMemoryPropertyFlags properties ,VkImage& image ,VkDeviceMemory& imageMemory){
+    VkImageCreateInfo imageInfo{};
+    imageInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+    imageInfo.imageType = VK_IMAGE_TYPE_2D;
+    imageInfo.extent.width = width;
+    imageInfo.extent.height = height ;
+    imageInfo.extent.depth = 1;
+    imageInfo.mipLevels = 1;
+    imageInfo.arrayLayers = 1;
+    imageInfo.format = format;
+    imageInfo.tiling = tiling ; 
+    imageInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+    imageInfo.usage = usage;
+    imageInfo.samples = VK_SAMPLE_COUNT_1_BIT;
+    imageInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+    if (vkCreateImage (device , &imageInfo , nullptr , &image)!= VK_SUCCESS){
+        throw std::runtime_error("failed to create an Image ");
+
+    }
+
+    VkMemoryRequirements memRequirements;
+    VkGetImageMemoryRequirements (device , image , &memRequirements);
+
+    VkMemoryAllocateInfo allocInfo{};
+    allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+    allocInfo.allocationSize = memRequirements.size;
+    allocInfo.memoryTypeIndex = findMemoryType(memRequirements.memoryTypeBits ,properties);
+    
+    if (vkAllocateMemory(device , &allocInfo , nullptr , &imageMemory)!= VK_SUCCESS){
+        throw std::runtime_error("failed to allocate Image memory");
+    }
+   vkBindImageMemory (device ,image , imageMemory , 0);
+ }
 void createVertexBuffer(){
  
     VkDeviceSize bufferSize = sizeof(vertices[0])* vertices.size();
