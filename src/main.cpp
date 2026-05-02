d0f8bfac76242852984eafddca9cad6da675726d
Avi <avishkarkale29@gmail.com>
2026-05-03 01:36:28 +0530

    Implemet Buffer-to-Image copy helper

--- src/main.cpp
+++ src/main.cpp
@@ -836,7 +836,7 @@ uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties){
 
  }
 
- VkCommandBuffer beginSingleTimeCommand(){
+ VkCommandBuffer beginSingleTimeCommands(){
     VkCommandBufferAllocateInfo allocInfo{};
     allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
     allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
@@ -937,7 +937,7 @@ uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties){
     }
 
     VkMemoryRequirements memRequirements;
-    VkGetImageMemoryRequirements (device , image , &memRequirements);
+    vkGetImageMemoryRequirements (device , image , &memRequirements);
 
     VkMemoryAllocateInfo allocInfo{};
     allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
@@ -950,34 +950,34 @@ uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties){
    vkBindImageMemory (device ,image , imageMemory , 0);
  }
 
- void transitionImageLayout(VkImage image , VkFormat format , VkImageLayout oldLayout , VkImageLayout , newLayout){
-    VkCommandBuffer commandBuffer = beginSingleTimeCommand();
+ void transitionImageLayout(VkImage image , VkFormat format , VkImageLayout oldLayout , VkImageLayout  newLayout){
+    VkCommandBuffer commandBuffer = beginSingleTimeCommands();
     VkImageMemoryBarrier barrier{};
-    barrierInfo.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
-    barrierInfo.oldLayout = oldLayout;
-    barrierInfo.newLayout = newLayout;
-    barrierInfo.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
-    barrierInfo.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
-    barrierInfo.image = image;
+    barrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
+    barrier.oldLayout = oldLayout;
+    barrier.newLayout = newLayout;
+    barrier.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+    barrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+    barrier.image = image;
 
-    barrierInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
-    barrierInfo.subresourceRange.baseMipLevel=0;;
-    barrierInfo.subresourceRange.levelCount=1;
-    barrierInfo.subresourceRange.baseArrayLayer=0;
-    barrierInfo.subresourceRange.layerCount =1;
+    barrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+    barrier.subresourceRange.baseMipLevel=0;;
+    barrier.subresourceRange.levelCount=1;
+    barrier.subresourceRange.baseArrayLayer=0;
+    barrier.subresourceRange.layerCount =1;
 
-    VkPipelineStageFlags = sourceStage;
-    VkPipelineStageFlags = destinationStage;
+    VkPipelineStageFlags  sourceStage;
+    VkPipelineStageFlags  destinationStage;
 
-    if (oldLayout == VK_IMAGE_LAYOUT_UNDEFINED && newLayout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIONAL){
-        barrier.srcAcessMask = 0;
+    if (oldLayout == VK_IMAGE_LAYOUT_UNDEFINED && newLayout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL){
+        barrier.srcAccessMask = 0;
         barrier.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
 
         sourceStage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
         destinationStage = VK_PIPELINE_STAGE_TRANSFER_BIT;
     }
-    else if (oldLayout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIONAL && newLayout ==VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIONAL){
-         barrier.srcAcessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+    else if (oldLayout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL && newLayout ==VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL){
+        barrier.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
         barrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
 
         sourceStage = VK_PIPELINE_STAGE_TRANSFER_BIT;
@@ -988,9 +988,9 @@ uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties){
 
    }
 
-   VkCmdPipelineBarrier(
+   vkCmdPipelineBarrier(
     commandBuffer,
-    source stage , destinationStage,
+    sourceStage , destinationStage,
     0,
     0, nullptr,
     0, nullptr,
@@ -999,6 +999,33 @@ uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties){
 
    endsingleTimeCommands(commandBuffer);
 
+ }
+
+ void copyBufferToImage(VkBuffer buffer , VkImage image ,uint32_t width , uint32_t height){
+
+    VkCommandBuffer commandBuffer = beginSingleTimeCommands();
+    VkBufferImageCopy region {};
+    region.bufferOffset = 0;
+    region.bufferRowLength = 0;
+    region.bufferImageHeight = 0;
+
+    region.imageSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+    region.imageSubresource.mipLevel=0;
+    region.imageSubresource.baseArrayLayer=0;
+    region.imageSubresource.layerCount=1;
+
+    region.imageOffset={0,0,0};
+    region.imageExtent={width, height , 1};
+
+    vkCmdCopyBufferToImage(
+        commandBuffer,
+        buffer,
+        image,
+        VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+        1,
+        &region
+    );
+    endsingleTimeCommands(commandBuffer);
  }
 void createVertexBuffer(){
  
